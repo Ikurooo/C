574f805f21efae3b28016e260bbb81a5f55cf219
ivan <ivanecankov@gmail.com>
2023-10-31 23:15:48 +0100

    added doxygen style docs

--- mycompress.c
+++ mycompress.c
@@ -1,17 +1,41 @@
+/**
+ * @file mycompress.c
+ * @author Ivan Cankov 12219400 <e12219400@student.tuwien.ac.at>
+ * @date 31.10.2023
+ *
+ * @brief A C program for compressing different files.
+ **/
+
 #include <assert.h>
-#include <errno.h>
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
+/**
+ * @brief Print a usage message to stderr and exit the process with EXIT_FAILURE.
+ *
+ * @param program_name The name of the current program.
+ */
 void usage(const char *program_name) {
     fprintf(stderr, "[%s] Usage: %s [-o outfile] [file...]\n", program_name, program_name);
     exit(EXIT_FAILURE);
 }
 
+
+/**
+ * @brief Compresses the input file and writes it out to the output file.
+ * It additionally modifies the amount of characters read and written.
+ *
+ * @details It is assumed that all pointers passed in are valid pointers.
+ * Will throw a segfault if invalid.
+ * @param in The input file to be compressed.
+ * @param out The output file to write the compressed elements to.
+ * @param read Pointer to the read characters counter.
+ * @param written Pointer to the written characters counter.
+ * @return 0 on success, -1 otherwise.
+ */
 int compress(FILE *in, FILE *out, uint16_t *read, uint16_t *written) {
     char *line = NULL;
     size_t size = 0;
@@ -50,12 +74,12 @@ int compress(FILE *in, FILE *out, uint16_t *read, uint16_t *written) {
         }
 
         error = fprintf(out, "\n");
-        *read += 1;
         if (error < 0) {
             return -1;
         }
-        *written += error;
 
+        *read += error;
+        *written += error;
         free(line);
         line = NULL;
         size = 0;
@@ -63,6 +87,13 @@ int compress(FILE *in, FILE *out, uint16_t *read, uint16_t *written) {
 
     return 0;
 }
+
+/**
+ * @brief Entrypoint; error handling and file opening/closing is done here.
+ * @param argc
+ * @param argv
+ * @return EXIT_SUCCESS on success, otherwise EXIT_FAILURE.
+ **/
 
 int main(int argc, char *argv[]) {
     const char *program_name = argv[0];
@@ -97,7 +128,6 @@ int main(int argc, char *argv[]) {
 
     int length = argc - optind;
     char *filenames[length];
-    FILE *infile = NULL;
 
     for (int i = 0; i < length; i++) {
         filenames[i] = argv[optind + i];
@@ -105,23 +135,19 @@ int main(int argc, char *argv[]) {
 
     uint16_t read = 0;
     uint16_t written = 0;
+    FILE *infile = NULL;
     const char *infile_name = NULL;
 
     for (int i = 0; i < length; i++) {
         infile_name = filenames[i];
         infile = fopen(infile_name, "r");
         if (infile == NULL) {
-            fprintf(stderr, "[%s] ERROR: An error occurred while opening file %s\n", program_name, infile_name);
-            fclose(outfile);
-            exit(EXIT_FAILURE);
+            goto SINGLE_FILE;
         }
 
         int error = compress(infile, outfile, &read, &written);
         if (error < 0) {
-            fprintf(stderr, "[%s] ERROR: An error occurred while compressing file %s\n", program_name, infile_name);
-            fclose(outfile);
-            fclose(infile);
-            exit(EXIT_FAILURE);
+            goto BOTH_FILES;
         }
     }
 
@@ -129,10 +155,8 @@ int main(int argc, char *argv[]) {
         infile = stdin;
         int error = compress(infile, outfile, &read, &written);
         if (error != 0) {
-            fprintf(stderr, "[%s] ERROR: An error occurred while compressing stdin [%d]\n", program_name, error);
-            fclose(outfile);
-            fclose(infile);
-            exit(EXIT_FAILURE);
+            fprintf(stderr, "[%s] ERROR: An error occurred while compressing stdin\n", program_name);
+            goto BOTH_FILES;
         }
     }
 
@@ -143,4 +167,11 @@ int main(int argc, char *argv[]) {
     printf("Written: %d\n", written);
     printf("Read: %d\n", read);
     return EXIT_SUCCESS;
+
+    BOTH_FILES:
+    fprintf(stderr, "[%s] ERROR: An error occurred while compressing file %s\n", program_name, infile_name);
+    fclose(infile);
+    SINGLE_FILE:
+    fclose(outfile);
+    exit(EXIT_FAILURE);
 }
