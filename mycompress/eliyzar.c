3aee576b1f285d5093e59b49fd734457089c8890
ikurooo <IvaneCankov@proton.me>
2023-11-07 23:22:29 +0100

    compacted to 89 lines...

--- mycompress/eliyzar.c
+++ mycompress/eliyzar.c
@@ -2,97 +2,87 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <assert.h>
 
 void usage(const char *program_name) {
     fprintf(stderr, "[%s] Usage: %s [-o outfile] [file...]\n", program_name, program_name);
     exit(EXIT_FAILURE);
 }
 
-// Function to compress and write data
-void compressAndWrite(char *buffer, size_t len, FILE *outFile, int *writeCount) {
-    u_char lastChar;
-    u_char currentChar;
-    int count = 0;
+void compressAndWrite(char *buffer, FILE *outFile, int *writeCount) {
+    char lastChar = buffer[0];
+    int count = 1;
 
-    for (int i = 0; i < len; ++i) {
-
-        currentChar = buffer[i];
-
-        if (count == 0) {
-            lastChar = currentChar;
-            count = 1;
-            continue;
-        }
-
-        if (lastChar == currentChar) {
+    for (int i = 1; buffer[i] != '\0'; ++i) {
+        if (lastChar == buffer[i]) {
             count++;
-            continue;
+        } else {
+            int written = fprintf(outFile, "%c%d", lastChar, count);
+            if (written == -1) {
+                exit(EXIT_FAILURE);
+            }
+            *writeCount += written;
+            lastChar = buffer[i];
+            count = 1;
         }
-
-        int written = fprintf(outFile, "%c%d", lastChar, count);
-        if (written == -1) {
-            exit(EXIT_FAILURE);
-        }
-
-        *writeCount += written;
-        lastChar = currentChar;
-        count = 1;
     }
-    int written = fprintf(outFile, "\n");
-    *writeCount += written;
+
+    fprintf(outFile, "\n");
+    *writeCount += 1;
 }
 
 int main(int argc, char *argv[]) {
     const char *process = argv[0];
-    FILE *outFile = stdout;
-
     const char *inFileName = NULL;
     int opt;
 
-    // "o:ab" -> o can have an argument after it a and b can't
     while ((opt = getopt(argc, argv, "o:")) != -1) {
-        switch (opt) {
-            case 'o':
-                if (inFileName != NULL) {
-                    usage(process);
-                }
-                inFileName = optarg;
-                break;
-
-                // anything that isn't specified in the last param of getopt()
-            case '?':
-                usage(process);
-                break;
-                // unreachable code
-            default:
-                assert(0);
+        if (opt == 'o' && inFileName == NULL) {
+            inFileName = optarg;
+        } else {
+            usage(process);
         }
     }
 
-    FILE *inFile = stdin;
-    // add necessary checks to open the outfile
-
-    char *line = NULL;
-    size_t size = 0;
-    int readCount = 0, writeCount = 0;
-
-    // saves a line up until \n into line !!DYNAMICALLY free() needed
-    while (getline(&line, &size, inFile) != -1) {
-        compressAndWrite(line, strlen(line), stdout, &writeCount);
-        readCount += (int) strlen(line);
+    FILE *outFile = stdout;
+    if (inFileName != NULL) {
+        outFile = fopen(inFileName, "w");
+        if (outFile == NULL) {
+            fprintf(stderr, "[%s] ERROR: An error occurred while opening file %s\n", process, inFileName);
+            exit(EXIT_FAILURE);
+        }
     }
-    free(line);
 
-    // Cleanup
+    int readCount = 0;
+    int writeCount = 0;
+
+    for (int i = optind; i < argc; ++i) {
+        FILE *inFile = fopen(argv[i], "r");
+        if (inFile == NULL) {
+            fprintf(stderr, "[%s] ERROR: An error occurred while opening file %s\n", process, argv[i]);
+            if (outFile != stdout) {
+                fclose(outFile);
+            }
+            exit(EXIT_FAILURE);
+        }
+
+        char *line = NULL;
+        size_t size = 0;
+
+        while (getline(&line, &size, inFile) != -1) {
+            compressAndWrite(line, outFile, &writeCount);
+            readCount += (int) strlen(line);
+        }
+        free(line);
+
+        fclose(inFile);
+    }
+
     if (outFile != stdout) {
         fclose(outFile);
     }
 
-    // Print character counts to stderr
     fprintf(stderr, "Read: %d characters\n", readCount);
     fprintf(stderr, "Written: %d characters\n", writeCount);
 
     return EXIT_SUCCESS;
 }
-
