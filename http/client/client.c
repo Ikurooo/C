9919b5dcce00402a7cafb4225cbea54b09319888
ivan <ivanecankov@gmail.com>
2024-01-09 18:43:28 +0100

    ../util.h

--- http/client/client.c
+++ http/client/client.c
@@ -5,20 +5,7 @@
  * @brief A simple HTTP client in C
  **/
 
-#define _GNU_SOURCE
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-#include <stdbool.h>
-#include <assert.h>
-#include <string.h>
-#include <errno.h>
-#include <sys/socket.h>
-#include <netdb.h>
-#include <arpa/inet.h>
-#include <math.h>
-#include <limits.h>
-#include <sys/stat.h>
+#include "../util.h"
 
 typedef struct {
     char *file;
